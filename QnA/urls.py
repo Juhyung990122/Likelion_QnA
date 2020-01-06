7fe12e99be1e1e9f87da71dcedb4fd9c363d2ae0
Guinness990122 <harukawa192049@gmail.com>
2020-01-06 21:40:46 +0900

    Revise Imageupload

--- QnA/urls.py
+++ QnA/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('question',views.QuestionVeiwSet)
@@ -8,4 +10,4 @@ router.register('answer',views.AnswerViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
