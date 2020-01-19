3187219f57bcff93d6ca13e3d397d9c910616fbe
Guinness990122 <harukawa192049@gmail.com>
2020-01-19 23:57:11 +0900

    Init Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <img src = {logo} align ="left"></img>
+      <h3 align = "left">SMU LIKELION_QNA</h3>
+      <ul>
+        <li>Notice</li>
+        <li>QnA</li>
+        <li>Login</li>
+        <li>Signup</li>
+      </ul>
     </div>
+   
   );
 }
 
