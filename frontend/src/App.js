37250c99b77eaf50e54ff6c84faa381691ff007d
Guinness990122 <harukawa192049@gmail.com>
2020-01-20 17:32:05 +0900

    Add Cover,Animated mouse scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.png'
+import logo from './images/logo.png'
+import background from './images/background.png'
 
 function App() {
   return (
-    <div className="header">
-      <img src = {logo} align ="left"></img>
-      <h3 align = "left">SMU LIKELION_QNA</h3>
-      <ul>
-        <li>Notice</li>
-        <li>QnA</li>
-        <li>Login</li>
-        <li>Signup</li>
-      </ul>
+    <div className="index">
+      <div className="Header">
+        <img id='logo' src = {logo}></img>
+        <h3 id ='smu_likelion_qna'>SMU LIKELION_QNA</h3>
+        <ul id = 'menu'>
+          <li>Notice</li>
+          <li>QnA</li>
+          <li>Login</li>
+          <li>Signup</li>
+        </ul>
+      </div>
+      <div className='background'>
+      </div>
+      <div class="scroll-downs">
+  <div class="mousey">
+    <div class="scroller"></div>
+  </div>
+</div>
     </div>
-   
   );
 }
 
