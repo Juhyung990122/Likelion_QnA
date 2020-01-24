38ef5832ff8e05058f708b4e3854b01924a2de9f
Guinness990122 <harukawa192049@gmail.com>
2020-01-24 16:22:54 +0900

    Allow hosts

--- backend/Likelion_QnA/settings.py
+++ backend/Likelion_QnA/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'pj%+nhp=^!g)g*togtmy3#zp@qv)$a
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
