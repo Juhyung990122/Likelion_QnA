66fee9392be983d396b3bf7ebeedba74f98eae17
Guinness990122 <harukawa192049@gmail.com>
2020-02-10 23:36:00 +0900

    Add Pagination

--- frontend/src/containers/Question.js
+++ frontend/src/containers/Question.js
@@ -1,22 +1,53 @@
 import React,{Component} from 'react';
 import Content from '../components/Content';
 import * as service from '../qna';
+import '../components/containers_css/Question.css';
 
 
 class QuestionList extends Component {
-    id = 1
-    state = {
-        fetching: false,
-        questionlist:[   
-            {
-            author_name: null,
-            title: null,
-            date: null,
-            image: null,
-            }  
-        ],
-        keyword:''
+    constructor(props){
+        super(props);
+        this.state = {
+            page_num : 1,
+            fetching: false,
+            questionlist:[   
+                {
+                author_name: null,
+                title: null,
+                date: null,
+                image: null,
+                },  
+                {
+                    author_name: null,
+                    title: null,
+                    date: null,
+                    image: null,
+                },
+                {
+                    author_name: null,
+                    title: null,
+                    date: null,
+                    image: null,
+                }, 
+                {
+                    author_name: null,
+                    title: null,
+                    date: null,
+                    image: null,
+                }, 
+                {
+                    author_name: null,
+                    title: null,
+                    date: null,
+                    image: null,
+                }  
+            ],
+        }
+        this.nextpage = this.nextpage.bind(this);
+        this.prevpage = this.prevpage.bind(this);
     }
+    id = 1
+
 
     fetchQuestionInfo = async(page_num) => { 
         this.setState({
@@ -28,29 +59,64 @@ class QuestionList extends Component {
         const questionlist = questionlistinfo.data.results
 
         this.setState({
+            page_num,
             questionlist,
             fetching:false
         })
-        
     }
     
     componentDidMount() {
-        this.fetchQuestionInfo(1);
-        
+        this.fetchQuestionInfo(this.state.page_num); 
     }
 
+    nextpage(){
+        this.fetchQuestionInfo(this.state.page_num+1); 
+    }
+
+    prevpage(){
+        if(this.state.page_num <= 1){
+            alert("첫페이지입니다.")
+        }    
+        else{
+            this.fetchQuestionInfo(this.state.page_num-1); 
+        }
+            
+    }
    
     render() {
         const question = this.state
         var questionList = question.questionlist;
-        console.log(questionList)
         return (
             <div>
                 <Content>
-                <li>{questionList[0].title}</li>
-                <li>{questionList[0].title}</li>
-                <li>{questionList[0].title}</li>
-                <li>{questionList[0].title}</li>
+                    <div className='table'>
+                    <table border='1'>
+                        <tr >
+                        <td>이미지</td><td>제목</td><td>등록일</td>
+                        </tr>
+                        <tr>
+                        <td><img href={questionList[0].image}/></td><td>{questionList[0].title}</td><td>{questionList[0].date}</td>
+                        </tr>
+                        <tr>
+                        <td>이미지</td><td>{questionList[1].title}</td><td>{questionList[1].date}</td>
+                        </tr>
+                        <tr>
+                        <td>이미지</td><td>{questionList[2].title}</td><td>{questionList[2].date}</td>
+                        </tr>
+                        <tr>
+                        <td>이미지</td><td>{questionList[3].title}</td><td>{questionList[3].date}</td>
+                        </tr>
+                        <tr>
+                        <td>이미지</td><td>{questionList[4].title}</td><td>{questionList[4].date}</td>
+                        </tr>
+
+                    </table>
+                    </div>
+                    <div className='pagination'>
+                    <button onClick={this.prevpage}>Prev</button>
+                    <button onClick={this.nextpage}>Next</button>
+                    </div>
+                    
                 </Content>
             </div>
         );
