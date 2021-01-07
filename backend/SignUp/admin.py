b8ecd6acef6ccae1e301023c0b354b6e86a62a3c
Juhyung990122 <harukawa99@naver.com>
2021-01-07 20:51:52 +0900

    Tidy up project

--- backend/SignUp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import LionUser
-from .models import MgmtUser
-# Register your models here.
-
-admin.site.register(LionUser)
-admin.site.register(MgmtUser)
